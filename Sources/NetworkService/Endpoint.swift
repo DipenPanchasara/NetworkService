d5841f0cc02b6faa22ab1b9d34198f89680d11ff
DipenPanchasara <dipenapanchasara@gmail.com>
2024-05-20 17:40:24 +0100

    made endpoint init public

--- Sources/NetworkService/Endpoint.swift
+++ Sources/NetworkService/Endpoint.swift
@@ -10,4 +10,9 @@ import Foundation
 public struct Endpoint: Equatable, Hashable {
   let path: String
   var queryItems: [String: String]?
+  
+  public init(path: String, queryItems: [String : String]? = nil) {
+    self.path = path
+    self.queryItems = queryItems
+  }
 }
